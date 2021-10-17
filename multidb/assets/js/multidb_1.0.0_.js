2fe8725b78698531343d0a9866c9652c2ff482bd
Ubuntu <ubuntu@ip-172-31-32-7.us-east-2.compute.internal>
2021-10-16 19:30:49 -0500

    Bk

---
+++ multidb/assets/js/multidb_1.0.0_.js
@@ -0,0 +1,12 @@
+/*
+	Scripts for MultiDb
+	By: Moisés Castillo
+	Date: 2021-09-18
+	mail: moidcr@gmail.com
+	versión 1.0.0
+*/
+
+shbtndbmy = function()
+{
+	
+}
---
+++ userscustom/ChangeLog.md
@@ -0,0 +1,5 @@
+# CHANGELOG USERSCUSTOM FOR [DOLIBARR ERP CRM](https://www.dolibarr.org)
+
+## 1.0
+
+Initial version
